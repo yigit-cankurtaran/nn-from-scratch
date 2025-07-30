60fdc26f650c278f9e8a962eb1183144ec50fd4d
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-07-30 20:18:43 +0300

    learning about how to create vectors from lists and transposing them

---
+++ list_to_matrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = [1, 2, 3]
+
+b = np.array([a])
+
+print(f"a after array is {b}")
+print(f"shape of a after array is {np.shape(b)}")
+
+c = np.transpose(b)
+print(f"b after transpose {c}")
+print(f"shape of a after transpose is {np.shape(c)}")
