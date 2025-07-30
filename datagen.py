51c6b5d6921924c17ae46632ed5b44500217b6bf
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-07-30 21:26:16 +0300

    first data generation

---
+++ datagen.py
@@ -0,0 +1,12 @@
+from nnfs.datasets import spiral_data
+import nnfs
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg') # prevention from opening a window
+
+nnfs.init()
+
+X, y = spiral_data(samples=100, classes=3)
+plt.scatter(X[:,0], X[:,1])
+plt.savefig("spiral_scatter.png")
