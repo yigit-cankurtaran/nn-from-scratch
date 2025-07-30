d919722b3e5dc31a1b2b0eff90e0321a9565dab7
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-07-30 21:27:17 +0300

    updated data generation

--- datagen.py
+++ datagen.py
@@ -8,5 +8,5 @@ matplotlib.use('Agg') # prevention from opening a window
 nnfs.init()
 
 X, y = spiral_data(samples=100, classes=3)
-plt.scatter(X[:,0], X[:,1])
+plt.scatter(X[:,0], X[:,1], c=y, cmap='brg') #color yes color map blue red green
 plt.savefig("spiral_scatter.png")
