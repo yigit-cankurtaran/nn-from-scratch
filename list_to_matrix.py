d287ba6fdbf7a8b86e71c131a71a8d83ab8de9d6
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-07-30 20:19:59 +0300

    learned a new way of transpose

--- list_to_matrix.py
+++ list_to_matrix.py
@@ -10,3 +10,8 @@ print(f"shape of a after array is {np.shape(b)}")
 c = np.transpose(b)
 print(f"b after transpose {c}")
 print(f"shape of a after transpose is {np.shape(c)}")
+
+d = b.T # another way of getting transpose
+print(f"d is {d}")
+print(f"shape of d is {np.shape(d)}")
+
