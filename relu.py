fdf2996e07cd851609622089f2f1d7a02b992450
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-07-31 17:33:05 +0300

    simpler relu code

--- relu.py
+++ relu.py
@@ -3,10 +3,6 @@ inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
 outputs = []
 
 for i in inputs:
-    if i < 0:
-        i = 0
-    else:
-        i = i
-    outputs.append(i)
+    outputs.append(max(0, i))
 
 print(outputs)
