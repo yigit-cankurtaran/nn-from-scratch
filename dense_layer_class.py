a0660575f0d08725843b44be873a8323849c437a
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-08-03 17:19:55 +0300

    implemented Loss and CrossEntropyLoss classes

--- dense_layer_class.py
+++ dense_layer_class.py
@@ -34,6 +34,30 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+#later we'll add more code to this
+class Loss:
+    def calculate(self, output, y):
+        # sample losses
+        sample_losses = self.forward(output, y)
+
+        #mean loss
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategoricalCrossEntropy(Loss): #inherits Loss class
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+
+        # clip data to prevent numerical issues
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
 
 X, y = spiral_data(samples=100, classes=3)
 dense1 = Layer_Dense(2, 3)
