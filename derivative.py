08ad7999ac10b9aff1e445f7db7839972524442d
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-08-08 23:40:57 +0300

    tangent line on function with graphics

--- derivative.py
+++ derivative.py
@@ -4,26 +4,27 @@ import matplotlib.pyplot as plt
 def f(x):
     return 2*x**2
 
-x = np.array(range(5))
+x = np.arange(0, 5, 0.001)
 y = f(x)
 
 plt.plot(x,y)
-plt.show()
-
-print(x)
-print(y)
 
 h = 0.0000001
 
-for num in x:
-    derivative = (f(x + h) - f(x)) / h
-    print(f"derivative of f({num}) is {derivative}")
-
-x1 = 1
+x1 = 2
 x2 = x1 + h
 y1 = f(x1)
 y2 = f(x2)
 
-print(f"new derivative is {(y2 - y1) / (x2 - x1)}")
-# prints something like "new derivative is 4.000000199840144"
-# derivative of 2x^2 is 4x
+approx_derivative = (y2 - y1) / (x2 - x1) # x2 - x1 is just h
+b = y2 - approx_derivative*x2 # y = mx + b, m is derivative bc slope=rate of change
+
+def tangent_line(x):
+    return approx_derivative*x + b # mx+b again
+
+to_plot = [x1-0.9, x1, x1+0.9]
+print(f"approx derivative for f(x) where x = {x1} is {approx_derivative}")
+plt.plot(to_plot, [tangent_line(i) for i in to_plot])
+plt.plot(x1, y1, 'ro') # point at x1
+plt.plot(x2, y2, 'go') # point looks green bc they're "impossibly" close
+plt.show()
