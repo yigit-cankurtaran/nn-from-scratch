8a9c7c44b48ea337d48e8686d40439edc952ee04
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-08-11 16:54:20 +0300

    derivative of multiple values

--- derivative.py
+++ derivative.py
@@ -9,22 +9,28 @@ y = f(x)
 
 plt.plot(x,y)
 
-h = 0.0000001
+colors = ["k", "g", "r", "b", "c"]
 
-x1 = 2
-x2 = x1 + h
-y1 = f(x1)
-y2 = f(x2)
+def approx_tan(x, approx_derivative):
+    return (approx_derivative*x) + b # mx + b again, derivative is slope
 
-approx_derivative = (y2 - y1) / (x2 - x1) # x2 - x1 is just h
-b = y2 - approx_derivative*x2 # y = mx + b, m is derivative bc slope=rate of change
+for i in range(5):
+    h = 0.0000001
+    
+    x1 = i
+    x2 = x1 + h
+    y1 = f(x1)
+    y2 = f(x2)
 
-def tangent_line(x):
-    return approx_derivative*x + b # mx+b again
+    print(f"current coords are {(x1, y1)} and {(x2, y2)}")
+    approx_derivative = (y2 - y1) / (x2 - x1) # x2 - x1 is just h
+    b = y2 - approx_derivative*x2 # y = mx + b, m is derivative bc slope=rate of change
+        
+    to_plot = [x1-0.9, x1, x1+0.9]
 
-to_plot = [x1-0.9, x1, x1+0.9]
-print(f"approx derivative for f(x) where x = {x1} is {approx_derivative}")
-plt.plot(to_plot, [tangent_line(i) for i in to_plot])
-plt.plot(x1, y1, 'ro') # point at x1
-plt.plot(x2, y2, 'go') # point looks green bc they're "impossibly" close
+    plt.scatter(x1, y1, c=colors[i])
+
+    plt.plot([point for point in to_plot], [approx_tan(point, approx_derivative) for point in to_plot], c=colors[i])
+    print(f"approx derivative for f(x) where x = {x1} is {approx_derivative}")
+
 plt.show()
