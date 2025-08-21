a8931075d2cc1a22a7804180ba7d9820e512fc4e
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-08-21 17:25:54 +0300

    timing both separate and combined gradients

--- backprop.py
+++ backprop.py
@@ -1,5 +1,6 @@
 import numpy as np
 import nnfs
+from timeit import timeit
 from dense_layer_class import Activation_Softmax_Loss_CCE, Activation_Softmax, Loss_CategoricalCrossEntropy
 
 nnfs.init()
@@ -10,19 +11,29 @@ softmax_out = np.array([[0.7,0.1,0.2],
 class_targets = np.array([0,1,1]) # correct outputs
 
 # using our combined loss func
-softmax_loss = Activation_Softmax_Loss_CCE()
-softmax_loss.backward(softmax_out, class_targets)
-dvalues1 = softmax_loss.dinputs
+def f1():
+    softmax_loss = Activation_Softmax_Loss_CCE()
+    softmax_loss.backward(softmax_out, class_targets)
+    dvalues1 = softmax_loss.dinputs
+    return dvalues1
 
 # taking them separately
-activation = Activation_Softmax()
-activation.output = softmax_out
-loss = Loss_CategoricalCrossEntropy()
-loss.backward(class_targets, softmax_out)
-activation.backward(loss.dinputs)
-dvalues2 = activation.dinputs
+def f2():
+    activation = Activation_Softmax()
+    activation.output = softmax_out
+    loss = Loss_CategoricalCrossEntropy()
+    loss.backward(class_targets, softmax_out)
+    activation.backward(loss.dinputs)
+    dvalues2 = activation.dinputs
+    return dvalues2
 
-print(f"gradient of combined is \n {dvalues1}")
-print(f"gradient of separate is \n {dvalues2}")
+print(f"gradient of combined is \n {f1()}")
+print(f"gradient of separate is \n {f2()}")
+
+t1 = timeit(lambda: f1(), number=10000)
+t2 = timeit(lambda: f2(), number=10000)
+print(f"combined time is {t1}")
+print(f"separate time is {t2}")
+print(f"separate is {t2/t1} times slower")
 
 
