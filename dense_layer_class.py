bd3a41ea6c785de72d607440620b7232a9bf6711
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-08-31 16:57:26 +0300

    implemented adam optimizer

--- dense_layer_class.py
+++ dense_layer_class.py
@@ -229,6 +229,50 @@ class Optimizer_RMSprop():
         self.iterations += 1
     # in production optimizers the .step() method runs these 3 in order
 
+class Optimizer_Adam:
+    def __init__(self, learning_rate = 1e-3, decay=0., epsilon=1e-7, beta_1=0.9, beta_2=0.999):
+        # generally the default vals for Adam
+
+        self.learning_rate = learning_rate
+        self.current_lr = learning_rate
+        self.decay = decay # optional, 0 by default
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+
+        def pre_update_params(self):
+            if self.decay:
+                self.current_lr = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+
+        def update_params(self, layer):
+            if not hasattr(layer, 'weight_cache'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                layer.weight_cache = np.zeros_like(layer.weights)
+                layer.bias_momentums = np.zeros_like(layer.biases)
+                layer.bias_cache = np.zeros_like(layer.biases)
+
+            # update momentum
+            layer.weight_momentums = self.beta_1 * layer.weight_momentums + (1-self.beta_1) * layer.dweights
+            layer.bias_momentums = self.beta_1 * layer.bias_momentums + (1-self.beta_1) * layer.dbiases
+
+            # get corrected momentum, iteration starts from 0 and we need to raise it to 1
+            weight_momentums_corrected = layer.weight_momentums / (1-self.beta_1 ** (self.iterations+1))
+            bias_momentums_corrected = layer.bias_momentums / (1-self.beta_1 ** (self.iterations+1))
+            # update cache with squared current gradients
+            layer.weight_cache = self.beta_2 * layer.weight_cache + (1-self.beta_2) * layer.dweights**2
+            layer.bias_cache = self.beta_2 * layer.bias_cache + (1-self.beta_2) * layer.dbiases**2
+            #corrected cache
+            weight_cache_corrected = layer.weight_cache / (1-self.beta_2 ** (self.iterations+1))
+            bias_cache_corrected = layer.bias_cache / (1-self.beta_2 ** (self.iterations+1))
+
+            # sgd with normalization
+            layer.weights += -self.current_lr * weight_momentums_corrected / (np.sqrt(weight_cache_corrected) + self.epsilon)
+            layer.biases += -self.current_lr * bias_momentums_corrected / (np.sqrt(bias_cache_corrected) + self.epsilon)
+
+        def post_update_params(self):
+            self.iterations += 1
+
 X, y = spiral_data(samples=100, classes=3)
 dense1 = Layer_Dense(2, 64) # 2 inputs 64 outputs
 dense2 = Layer_Dense(64, 3) # 64 inputs 3 outputs
