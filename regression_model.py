bc9262fef7d85a4b03e6842a8c0e5150ec0cab89
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-12 11:53:58 +0300

    created linear activation

--- regression_model.py
+++ regression_model.py
@@ -7,3 +7,15 @@ nnfs.init()
 X, y = sine_data()
 plt.plot(X, y)
 plt.show()
+
+
+# regression models use linear activation for output
+class Activation_Linear:
+    def forward(self, inputs):
+        # x = y
+        self.inputs = inputs
+        self.output = inputs
+
+    def backward(self, dvalues):
+        # 1 * dvalues = dvalues
+        self.dinputs = dvalues.copy()
