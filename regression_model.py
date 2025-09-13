dd43d06eff57565c24d06288921c07cda1d01ffa
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-13 14:33:43 +0300

    implemented mean squared error class

--- regression_model.py
+++ regression_model.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import nnfs
 from nnfs.datasets import sine_data
+from dense_layer_class import Loss
 
 nnfs.init()
 
@@ -19,3 +21,19 @@ class Activation_Linear:
     def backward(self, dvalues):
         # 1 * dvalues = dvalues
         self.dinputs = dvalues.copy()
+
+
+# imported Loss class from the previous example we used, inherited here
+class Loss_MeanSquaredError(Loss):
+    def forward(self, y_pred, y_true):
+        return np.mean((y_true - y_pred) ** 2, axis=-1)
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        # num of outputs every sample, using the first sample to count them
+        outputs = len(dvalues[0])
+
+        # grad on values
+        self.dinputs = -2 * (y_true - dvalues) / outputs
+        # normalize grad
+        self.dinputs /= samples
