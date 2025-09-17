2f0746f980cb560eff4dd73c7e0b2f2ade60dc61
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-17 16:42:31 +0300

    removed my own stuff, added a new layer

--- regression_model.py
+++ regression_model.py
@@ -55,14 +55,14 @@ class Loss_MeanAbsoluteError(Loss):  # a.k.a. L1 loss
         self.dinputs = self.dinputs / samples
 
 
-dense1 = Layer_Dense(
-    1, 64, weightregl2=5e-4, biasregl2=5e-4
-)  # imported from dense_layer_class
+dense1 = Layer_Dense(1, 64)  # imported from dense_layer_class
 activation1 = Activation_ReLU()
-dense2 = Layer_Dense(64, 1)  # output
-activation2 = Activation_Linear()
+dense2 = Layer_Dense(64, 64)
+activation2 = Activation_ReLU()
+dense3 = Layer_Dense(64, 1)  # output
+activation3 = Activation_Linear()
 loss_function = Loss_MeanSquaredError()
-optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-5)
+optimizer = Optimizer_Adam()
 
 # using standard deviation with a random constant
 # so that the accuracy we use can adapt to the inputs at hand
@@ -76,12 +76,18 @@ for epoch in range(10001):
     activation1.forward(dense1.output)
     dense2.forward(activation1.output)
     activation2.forward(dense2.output)
+    dense3.forward(activation2.output)
+    activation3.forward(dense3.output)
 
-    data_loss = loss_function.calculate(activation2.output, y)
-    reg_loss = loss_function.reg_loss(dense1) + loss_function.reg_loss(dense2)
+    data_loss = loss_function.calculate(activation3.output, y)
+    reg_loss = (
+        loss_function.reg_loss(dense1)
+        + loss_function.reg_loss(dense2)
+        + loss_function.reg_loss(dense2)
+    )
     loss = data_loss + reg_loss
 
-    predictions = activation2.output
+    predictions = activation3.output
     accuracy = np.mean(np.absolute(predictions - y) < acc_precision)
 
     if not epoch % 100:  # same thing as epoch % 100 == 0
@@ -91,8 +97,10 @@ for epoch in range(10001):
         )
 
     # backward pass
-    loss_function.backward(activation2.output, y)
-    activation2.backward(loss_function.dinputs)
+    loss_function.backward(activation3.output, y)
+    activation3.backward(loss_function.dinputs)
+    dense3.backward(activation3.dinputs)
+    activation2.backward(dense3.dinputs)
     dense2.backward(activation2.dinputs)
     activation1.backward(dense2.dinputs)
     dense1.backward(activation1.dinputs)
@@ -101,6 +109,7 @@ for epoch in range(10001):
     optimizer.pre_update_params()
     optimizer.update_params(dense1)
     optimizer.update_params(dense2)
+    optimizer.update_params(dense3)
     optimizer.post_update_params()
 
 # visualizing
@@ -110,7 +119,9 @@ dense1.forward(X_test)
 activation1.forward(dense1.output)
 dense2.forward(activation1.output)
 activation2.forward(dense2.output)
+dense3.forward(activation2.output)
+activation3.forward(dense3.output)
 
 plt.plot(X_test, y_test)  # normal data
-plt.plot(X_test, activation2.output)  # normal data
+plt.plot(X_test, activation3.output)  # normal data
 plt.show()
