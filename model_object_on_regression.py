e79e139aec4d79491f0b68a746e30631ff907fa8
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-10-18 22:16:41 +0300

    created model, added some layers into it

--- model_object_on_regression.py
+++ model_object_on_regression.py
@@ -1,3 +1,39 @@
 from nnfs.datasets import sine_data
+from dense_layer_class import Layer_Dense, Activation_ReLU
+
+
+class Activation_Linear:
+    def forward(self, inputs):
+        # linear activation simply passes values through
+        self.inputs = inputs
+        self.output = inputs
+
+    def backward(self, dvalues):
+        # derivative wrt inputs is 1 so gradient flows unchanged
+        self.dinputs = dvalues.copy()
+
 
 X, y = sine_data()
+
+
+class Model:
+    def __init__(self):
+        # list of network objects
+        self.layers = []
+
+    def add(self, layer):
+        # adding objects to the model
+        self.layers.append(layer)
+
+
+model = Model()  # instantiate object
+
+# adding layers
+model.add(Layer_Dense(1, 64))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(64, 64))
+model.add(Activation_ReLU())
+model.add(Layer_Dense(64, 1))
+model.add(Activation_Linear())
+
+print(model.layers)
