61a8fc2cf9049e0a759d4fa4ff97f11188d2e54d
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-10-26 03:51:34 +0300

    created set method, set loss func and optimizer

--- model_object_on_regression.py
+++ model_object_on_regression.py
@@ -1,6 +1,6 @@
 from nnfs.datasets import sine_data
-from dense_layer_class import Layer_Dense, Activation_ReLU
-from regression_model import Activation_Linear
+from dense_layer_class import Layer_Dense, Activation_ReLU, Optimizer_Adam
+from regression_model import Activation_Linear, Loss_MeanSquaredError
 
 X, y = sine_data()
 
@@ -14,6 +14,14 @@ class Model:
         # adding objects to the model
         self.layers.append(layer)
 
+    def set(self, *, loss, optim):
+        """
+        to set loss function and optimizer
+        \* here means that anything after it are keywords, passed by names and vals
+        """
+        self.loss = loss
+        self.optim = optim
+
 
 model = Model()  # instantiate object
 
@@ -24,5 +32,9 @@ model.add(Layer_Dense(64, 64))
 model.add(Activation_ReLU())
 model.add(Layer_Dense(64, 1))
 model.add(Activation_Linear())
+
+model.set(
+    loss=Loss_MeanSquaredError(), optim=Optimizer_Adam(learning_rate=0.005, decay=1e-3)
+)
 
 print(model.layers)
